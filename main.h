36dc6aa9e8d51c96725b50d446e7d44d60d2f3a4
NantaelM77 <natnaelmehari77@gmail.com>
2021-10-04 05:45:58 -0700

    Create _putchar.c and Update main.h, _printf.c

--- main.h
+++ main.h
@@ -6,6 +6,28 @@
 #include <stdarg.h>
 #include <unistd.h>
 
+/**
+ * struct print - struct containing chrs for comparison with specfiers and
+ * selecting appropriate function
+ * @type: char to compare
+ * @funct: function to print
+ *
+ */
+
+typedef struct print
+{
+	char type;
+	int (*funct)(va_list);
+
+} print_f;
+
 int _printf(const char *format, ...);
+int _putchar(char c);
+int printNaN(char c1, char c2);
+
+int printchar(va_list a_list);
+int printstr(va_list a_list);
+int printint(va_list a_list);
+int (*_printfunc(char fi))(va_list);
 
 #endif
